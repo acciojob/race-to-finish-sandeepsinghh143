08a078a68ab87655cde434f698ca02572917e03f
Sandeep Singh <115461230+sandeepsinghh143@users.noreply.github.com>
2023-06-05 16:33:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-window.promises = [];
-
+window.promises = [new Promise(setTimeout((resolve,reject)=>{
+	resolve("a");
+},3000)),new Promise(setTimeout((resolve,reject)=>{
+	resolve("b");
+},1000)),new Promise(setTimeout((resolve,reject)=>{
+	resolve("c");
+},5000)),new Promise(setTimeout((resolve,reject)=>{
+	resolve("d");
+},4000)),new Promise(setTimeout((resolve,reject)=>{
+	resolve("e");
+},2000))];
+let div=document.getElementById("output")
+div.innerText=Promise.any(promises);
 // Do not change the code above this
 // add your promises to the array `promises`

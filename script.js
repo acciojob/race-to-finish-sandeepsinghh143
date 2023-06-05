ead92961e163042bb5c0dcb8a26ea2dad007f5ad
Sandeep Singh <115461230+sandeepsinghh143@users.noreply.github.com>
2023-06-05 16:41:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
-window.promises = [new Promise(setTimeout((resolve,reject)=>{
-	resolve("a");
-},3000)),new Promise(setTimeout((resolve,reject)=>{
-	resolve("b");
-},1000)),new Promise(setTimeout((resolve,reject)=>{
-	resolve("c");
-},5000)),new Promise(setTimeout((resolve,reject)=>{
-	resolve("d");
-},4000)),new Promise(setTimeout((resolve,reject)=>{
-	resolve("e");
-},2000))];
-let div=document.getElementById("output")
-div.innerText=Promise.any(promises);
+window.promises = [];
+// Array of promises
+
+// Function to generate a random time between min and max seconds
+function getRandomTime(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Create five promises with random timeouts
+for (let i = 0; i < 5; i++) {
+  const promise = new Promise((resolve) => {
+    const time = getRandomTime(1000, 5000);
+    setTimeout(() => {
+      resolve(`Promise ${i + 1} resolved after ${time / 1000} seconds.`);
+    }, time);
+  });
+  promises.push(promise);
+}
+
+// Use Promise.any() to wait for the first promise to resolve
+Promise.any(promises)
+  .then((result) => {
+    const outputDiv = document.getElementById('output');
+    outputDiv.innerText = result;
+  })
+  .catch((error) => {
+    console.log('All promises rejected:', error);
+  });
+
 // Do not change the code above this
 // add your promises to the array `promises`
